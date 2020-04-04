f4141738481363a5cf4da9d63f136f89c732eab7
Alexandre <jajrjunior95@gmail.com>
2020-04-04 17:18:23 -0300

    c3 - Criada uma classe para persistir usuarios no banco e criada uma classe para obter os usuarios persistidos

--- src/modelo/basico/Usuario.java
+++ src/modelo/basico/Usuario.java
@@ -1,12 +1,15 @@
 package modelo.basico;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Usuario {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
@@ -46,6 +49,13 @@ public class Usuario {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+	
+	
 	
 	
 	
---
+++ src/teste/basico/NovoUsuario.java
@@ -0,0 +1,32 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class NovoUsuario {
+	
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario user1 = new Usuario("Alexandre", "junior@teste.com.br");
+		Usuario user2= new Usuario("Vitor", "vitor@teste.com.br");
+		Usuario user3 = new Usuario("Tathi Crush", "tathi@teste.com.br");
+		
+		em.getTransaction().begin();
+		em.persist(user1);
+		em.persist(user2);
+		em.persist(user3);
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+
+		
+	}
+
+}
